70a5bf858c3a31693cf04da27639bc041908657c
shoab10 <shoab10@gmail.com>
2014-03-29 14:02:25 -0400

    sign in with nav bars

--- signin.php
+++ signin.php
@@ -1,17 +1,18 @@
 <?php
 $username="";
+$login_error="";
 include "function.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
   $check = user_pass_check($_POST['username'],$_POST['password']); // Call functions from function.php
       if($check == 1) {
         $login_error = "User ".$_POST['username']." not found.";
-        echo "$login_error";
+        //echo "$login_error";
         $username="";
       }
       elseif($check==2) {
         $login_error = "Incorrect password.";
-        echo "$login_error";
+        //echo "$login_error";
         $username=$_POST['username'];
       }
       else if($check==0){
@@ -43,6 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 
     <!-- Custom styles for this template -->
     <link href="/metube/css/signin.css" rel="stylesheet">
+    <link href="/metube/css/dashboard.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy this line! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
@@ -55,20 +57,69 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
   </head>
 
   <body>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#">Settings</a></li>
+            <li><a href="#">Profile</a></li>
+            <li><a href="#">Help</a></li>
+          </ul>
+          <form class="navbar-form navbar-right">
+            <input type="text" class="form-control" placeholder="Search...">
+          </form>
+        </div>
+      </div>
+    </div>
 
-    <div class="container">
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active"><a href="#">Overview</a></li>
+            <li><a href="#">Reports</a></li>
+            <li><a href="#">Analytics</a></li>
+            <li><a href="#">Export</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href="">Nav item</a></li>
+            <li><a href="">Nav item again</a></li>
+            <li><a href="">One more nav</a></li>
+            <li><a href="">Another nav item</a></li>
+            <li><a href="">More navigation</a></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li><a href="">Nav item again</a></li>
+            <li><a href="">One more nav</a></li>
+            <li><a href="">Another nav item</a></li>
+          </ul>
+        </div>
+      
+      <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main"> <!--Body Container-->
+      <div class="container">
 
       <form class="form-signin" role="form" action="signin.php" method="POST">
-        <h2 class="form-signin-heading">Please sign in</h2>
+        <h2 class="form-signin-heading">Sign In</h2>
         <input type="username" class="form-control" placeholder="Username" required autofocus name="username" value="<?php echo $username;?>">
         <input type="password" class="form-control" placeholder="Password" required name="password">
         <label class="checkbox">
           <input type="checkbox" value="remember-me"> Remember me
         </label>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        <button class="btn btn-primary btn-lg" type="submit">Sign in</button>
+        <p><?php echo $login_error; ?></p>
       </form>
-
-    </div> <!-- /container -->
+      </div> <!-- /container -->
 
 
     <!-- Bootstrap core JavaScript
