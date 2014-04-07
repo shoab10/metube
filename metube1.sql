27d3d5602a1a60ed697330761784c3b44c1c20e9
haseebmetube <haseeb050mjcc@gmail.com>
2014-04-07 11:09:21 -0400

    database updated

--- metube1.sql
+++ metube1.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 27, 2014 at 04:09 AM
+-- Generation Time: Apr 07, 2014 at 03:07 PM
 -- Server version: 5.6.12-log
 -- PHP Version: 5.4.16
 
@@ -45,6 +45,110 @@ INSERT INTO `account` (`accid`, `username`, `password`, `email`) VALUES
 (6, 'shoab101', '123456', 'shoab10@gmail.com'),
 (7, 'shoab10', '12345669', 'shoab@clemson.edu'),
 (9, 'haseeb101', '123456', 'ahaseeb@clemson.edu');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+CREATE TABLE IF NOT EXISTS `likes` (
+  `mediaid` int(20) NOT NULL,
+  `likeno` int(20) NOT NULL,
+  `views` int(20) NOT NULL,
+  UNIQUE KEY `mediaid` (`mediaid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `likes`
+--
+
+INSERT INTO `likes` (`mediaid`, `likeno`, `views`) VALUES
+(3, 0, 0),
+(8, 6, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `media`
+--
+
+CREATE TABLE IF NOT EXISTS `media` (
+  `mediaid` int(50) NOT NULL AUTO_INCREMENT,
+  `filename` varchar(50) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `keywoards` varchar(50) NOT NULL,
+  `filepath` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  PRIMARY KEY (`mediaid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
+
+--
+-- Dumping data for table `media`
+--
+
+INSERT INTO `media` (`mediaid`, `filename`, `type`, `title`, `keywoards`, `filepath`, `username`) VALUES
+(3, '624+assignment.png', 'image/png', 'hh', 'hh', 'uploads/shoab101/', 'shoab101'),
+(4, 'Video1.WMV', 'video/x-ms-wmv', 'kk', 'kk', 'uploads/shoab101/', 'shoab101'),
+(7, 'Video1.WMV', 'video/x-ms-wmv', 'kk', 'kk', 'uploads/shoab101/', 'shoab101'),
+(8, 'Video1.WMV', 'video/x-ms-wmv', 'kk', 'nn', 'uploads/shoab101/', 'shoab101');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `playlistmedia`
+--
+
+CREATE TABLE IF NOT EXISTS `playlistmedia` (
+  `pid` int(20) NOT NULL,
+  `mediaid` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `playlists`
+--
+
+CREATE TABLE IF NOT EXISTS `playlists` (
+  `pid` int(20) NOT NULL AUTO_INCREMENT,
+  `pname` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  PRIMARY KEY (`pid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `playlists`
+--
+
+INSERT INTO `playlists` (`pid`, `pname`, `username`) VALUES
+(3, 'music', 'shoab101'),
+(4, 'dance', 'shoab101'),
+(5, 'music', 'shoab101'),
+(6, 'dance', 'shoab101');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `upload`
+--
+
+CREATE TABLE IF NOT EXISTS `upload` (
+  `uploadid` int(50) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `mediaid` int(50) NOT NULL,
+  PRIMARY KEY (`uploadid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `upload`
+--
+
+INSERT INTO `upload` (`uploadid`, `username`, `mediaid`) VALUES
+(1, 'shoab101', 0),
+(2, 'shoab101', 0),
+(3, 'shoab101', 8);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
